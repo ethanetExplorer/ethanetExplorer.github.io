937917771907bb6078270d9cf127604e561253a2
ethanetexplorer <ethanetexplorer@outlook.com>
2024-07-31 21:47:35 +0800

    Changed gradient(How many times have you changed your gradient) Added random gradient generator in JS (Commented)

---
+++ script.js
@@ -0,0 +1,13 @@
+// function GetRandomGradient() {
+//   const red1 = Math.random() * 255;
+//   const green1 = Math.random() * 255;
+//   const blue1 = Math.random() * 255;
+
+//   const red2 = Math.random() * 255;
+//   const green2 = Math.random() * 255;
+//   const blue2 = Math.random() * 255;
+
+//   return `linear-gradient(to bottom, rgb(${red1}, ${green1}, ${blue1}), rgb(${red2}, ${green2}, ${blue2}))`;
+// }
+
+// document.body.style.background = GetRandomGradient();
